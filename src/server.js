25b48c905acd2b08cf23e4109f00d8000fa56c68
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2023-08-10 18:51:28 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,10 +8,10 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser())
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (req, res) {
     res.sendFile(
-        path.join(__dirname, "../../client/build/index.html"),
+        path.join(__dirname, "../client/build/index.html"),
         function (err) {
             res.status(500).send(err);
         }
